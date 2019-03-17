6f03464fcbb6abf9f8f5bef92d1860ba25b171a8
Patrik Kernstock <info@pkern.at>
2019-03-17 01:25:48 +0100

    Improved build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+MODE=$1
+
+# check dirty state.
+if ! git diff --no-ext-diff --quiet --exit-code; then
+	echo "We're in a dirty state. There are uncommitted changes. Please check!"
+	echo "Sleeping for 5 seconds before proceeding..."
+	sleep 5
+fi
 
 # parse info details
 echo "Parsing info file..."
@@ -17,5 +25,12 @@ git tag --force "v${INFO_VERSION}"
 # making a zip
 echo "Creating ZIP archive..."
 git archive --format=zip --prefix "${FOLDER_NAME}/" -o "${FOLDER_NAME}.zip" "v${INFO_VERSION}"
+
+# check if release mode.
+if [ "$MODE" == "release" ]; then
+	echo "Pushing git..."
+	git push --all
+	git push --tags
+fi
 
 echo "Done."
