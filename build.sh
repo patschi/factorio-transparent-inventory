6748973828d814f1f6373dda8f2b264fe3a6d11f
Patrik Kernstock <info@pkern.at>
2019-03-17 01:00:34 +0100

    Updated git files and added build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# parse info details
+echo "Parsing info file..."
+INFO_JSON=$(cat info.json)
+INFO_NAME=$(echo $INFO_JSON | jq --raw-output '.name')
+INFO_TITLE=$(echo $INFO_JSON | jq --raw-output '.title')
+INFO_VERSION=$(echo $INFO_JSON | jq --raw-output '.version')
+
+FOLDER_NAME=${INFO_NAME}_${INFO_VERSION}
+BUILD_DIR=/tmp/FACTORIO_MOD_${FOLDER_NAME}/
+
+# Let's create a git tag for this release
+echo "Tagging release..."
+git tag --force "v${INFO_VERSION}"
+
+# making a zip
+echo "Creating ZIP archive..."
+git archive --format=zip --prefix "${FOLDER_NAME}/" -o "${FOLDER_NAME}.zip" "v${INFO_VERSION}"
